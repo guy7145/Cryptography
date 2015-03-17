d74bc74b917f3c7106b564691b3eebc6259ff542
guy7145 <guy7145@gmail.com>
2015-03-17 17:35:27 +0200

    asdsadf

---
+++ Cryptography/AES_Cryptor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Cryptography
+{
+    class AES_Cryptor : ICryptography
+    {
+
+        public AES_Cryptor()
+        { 
+        }
+        public byte[] ICryptography.Encrypt()
+        { 
+
+        }
+        public string ICryptography.Decrypt()
+        { 
+
+        }
+    }
+}
