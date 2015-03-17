5f507e3742199e10e26d0e5579731dc93d22a50d
guy7145 <guy7145@gmail.com>
2015-03-17 16:36:48 +0200

    method names

---
+++ Cryptography/Cryptography.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography
+{
+    class Cryptography
+    {
+        private byte[] ByteSub(byte[] state)
+        {
+
+            return null;
+        }
+        private byte[] ShiftRow(byte[] state)
+        { 
+        }
+        private byte[] MixColumn(byte[] state)
+        {
+        }
+        private byte[] AddRoundKey(byte[] state, byte key)
+        { }
+    }
+}
