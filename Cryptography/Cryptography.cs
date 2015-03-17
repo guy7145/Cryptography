e20a1a7d0fdbce0d40d81a31bb23f432b34317db
gunrgnhsr <yonatannameri@outlook.com>
2015-03-17 17:11:55 +0200

    not changed

--- Cryptography/Cryptography.cs
+++ Cryptography/Cryptography.cs
@@ -12,12 +12,11 @@ namespace Cryptography
         private byte[] ByteSub(byte[] state)
         {
 
-            //אמא של יונתן מכינה אחלה חריימה
+            
             return null;
         }
         private byte[] ShiftRow(byte[] state)
         {
-            Console.WriteLine("אמא של יונתן עושה אחלה סטייקים");
         }
         private byte[] MixColumn(byte[] state)
         {
